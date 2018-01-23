62546777f225b5a94b569b3089c360e4553fe7f5
Will Smith <williedevonsmith@gmail.com>
2018-01-23 09:06:03 -0600

    New Commit

---
+++ KozmikSystems/Migrations/201801231443126_NavBarTableElements.cs
@@ -0,0 +1,16 @@
+namespace KozmikSystems.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class NavBarTableElements : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- KozmikSystems/Migrations/Configuration.cs
+++ KozmikSystems/Migrations/Configuration.cs
@@ -18,9 +18,9 @@ namespace KozmikSystems.Migrations
             context.Navbars.AddOrUpdate(
               x => x.Id,
               new Navbar() { Id = 1, nameOption = "Dashboard", controller = "Home", action = "Index", imageClass = "fa fa-dashboard fa-fw", status = true, isParent = false, parentId = 0 },
-              new Navbar() { Id = 2, nameOption = "Users", imageClass = "fa fa-users fa-fw", status = true, isParent = true, parentId = 0 },
-              new Navbar() { Id = 3, nameOption = "Add User", controller = "Users", action = "Create", status = true, isParent = false, parentId = 2 },
-              new Navbar() { Id = 4, nameOption = "View All Users", controller = "Users", action = "Index", status = true, isParent = false, parentId = 2 }
+              new Navbar() { Id = 2, nameOption = "Staff", imageClass = "fa fa-users fa-fw", status = true, isParent = true, parentId = 0 },
+              new Navbar() { Id = 3, nameOption = "Add Staff Member", controller = "Users", action = "Create", status = true, isParent = false, parentId = 2 },
+              new Navbar() { Id = 4, nameOption = "View All Staff Members", controller = "Users", action = "Index", status = true, isParent = false, parentId = 2 }
             );
         }
     }
